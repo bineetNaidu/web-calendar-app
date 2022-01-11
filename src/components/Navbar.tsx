999165d7dcff76d37ecbe4d53d1ea620431f88d9
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-11 10:40:17 +0530

    🎉 vamped Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,50 @@
-import { FC, useMemo } from 'react';
-import './Navbar.css';
-import { FaSearch, FaPlus, FaArrowDown } from 'react-icons/fa';
-import { Button } from '@chakra-ui/react';
+import { FC } from 'react';
+import {
+  Container,
+  Box,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Heading,
+  ButtonGroup,
+  IconButton,
+} from '@chakra-ui/react';
+import { FaUserCircle, FaSearch } from 'react-icons/fa';
+import { IoIosNotifications } from 'react-icons/io';
+import { HiDotsHorizontal } from 'react-icons/hi';
 
 const Navbar: FC = () => {
-  const getMonth = () => {
-    const date = new Date();
-    const month = date.getMonth();
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    return months[month];
-  };
-
-  const month = useMemo(() => getMonth(), []);
-
   return (
-    <nav className="nav">
-      <div className="nav__left">
-        <h1 className="nav__left__content">
-          <span className="nav__today">TODAY</span>
-          <span className="nav__dashed">|</span>
-          <span className="nav_month">{month}</span>
-          <Button size="xs" ml="6" bgColor="transparent">
-            <FaArrowDown />
-          </Button>
-        </h1>
-      </div>
-      <div className="nav__right">
-        <span className="nav__search">
-          <FaSearch />
-        </span>
-        <span className="nav__add">
-          <FaPlus />
-        </span>
-      </div>
-    </nav>
+    <Container
+      maxW="container.xl"
+      d="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      py="4"
+    >
+      <Box flex="3">
+        <Heading>Calendar</Heading>
+      </Box>
+      <Box flex="4">
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<FaSearch color="gray.300" />}
+          />
+          <Input placeholder="Search Something" />
+        </InputGroup>
+      </Box>
+      <Box flex="3" d="flex" justifyContent="flex-end">
+        <ButtonGroup spacing="4" size="md" variant="outline">
+          <IconButton
+            aria-label="Notifications"
+            icon={<IoIosNotifications />}
+          />
+          <IconButton aria-label="view morw" icon={<HiDotsHorizontal />} />
+          <IconButton aria-label="user" icon={<FaUserCircle />} />
+        </ButtonGroup>
+      </Box>
+    </Container>
   );
 };
 
