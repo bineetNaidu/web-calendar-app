ccfd23ec2243969698826a5d57f2773592f90a15
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-12 10:55:43 +0530

    🔥 add Google Signin Handler

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,15 +1,44 @@
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Container, Text, Button, Center } from '@chakra-ui/react';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { useUserStore } from '../lib/user.store';
+import { useNavigate } from 'react-router-dom';
 
 const Product: FC = () => {
+  const auth = getAuth();
+  const setUser = useUserStore((s) => s.setUser);
+  const navigate = useNavigate();
+
+  const handleGoogleAuth = async () => {
+    const res = await signInWithPopup(auth, new GoogleAuthProvider());
+    const user = res.user;
+    if (user) {
+      setUser({
+        id: user.uid,
+        name: user.displayName!,
+        email: user.email!,
+        photoURL: user.photoURL!,
+        created_at: '',
+      });
+      navigate('/');
+    }
+  };
+
   return (
-    <Box>
+    <Container py="8">
       <Text>Product</Text>
-      <Link to="/">
-        <Button>Home</Button>
-      </Link>
-    </Box>
+      <Button
+        w={'full'}
+        variant={'outline'}
+        leftIcon={<FcGoogle />}
+        onClick={handleGoogleAuth}
+      >
+        <Center>
+          <Text>Sign in with Google</Text>
+        </Center>
+      </Button>
+    </Container>
   );
 };
 
