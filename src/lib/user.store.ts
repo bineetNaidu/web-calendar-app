03725b812f2bad74aa45720f1bdb247670c1909e
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-12 10:34:29 +0530

    🎉 add User Store

---
+++ src/lib/user.store.ts
@@ -0,0 +1,22 @@
+import create from 'zustand';
+import { UserType } from './types';
+
+interface UserStoreType {
+  user: UserType & { isAuthenticated: boolean };
+  setUser: (user: UserType) => void;
+}
+
+export const useUserStore = create<UserStoreType>((set) => ({
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    created_at: '',
+    isAuthenticated: false,
+  },
+  setUser: (user) =>
+    set((state) => ({
+      ...state,
+      user: { ...user, isAuthenticated: true },
+    })),
+}));
