88b27060d83e28daac84e5e843a0613b304110cf
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-12 11:58:10 +0530

    🚀 add Auth State Change Listeners for better UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import Navbar from './components/Navbar';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useUserStore } from './lib/user.store';
+import { getAuth } from 'firebase/auth';
 
 type WrapperProps = {
   component: JSX.Element;
@@ -17,6 +19,27 @@ function App() {
       </>
     );
   };
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const { setUser } = useUserStore();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser({
+          created_at: '',
+          email: authUser.email!,
+          id: authUser.uid,
+          name: authUser.displayName!,
+          photoURL: authUser.photoURL!,
+        });
+        navigate('/');
+        return;
+      }
+      navigate('/product');
+    });
+  }, [auth, navigate, setUser]);
 
   return (
     <>
